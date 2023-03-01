50f54e9c0ef3c7ea0e2a978e16b4d88f3b84bb05
Seth Martin <sgm13555@gmail.com>
2023-03-01 15:49:03 -0600

    Code challenge 1

--- Section02/script.js
+++ Section02/script.js
@@ -1,6 +1,12 @@
-let firstName = "Bob";
-let lastName = "Dole";
-console.log(firstName + " " + lastName);
-console.log(firstName + " " + lastName);
-console.log(firstName + " " + lastName);
-console.log(firstName + " " + lastName);
+const massMark = 78;
+const heightMark = 1.69;
+
+const massJohn = 92;
+const heightJohn = 188;
+
+let markBmi = massMark / (heightMark * heightMark);
+let johnBmi = massJohn / (heightJohn * heightJohn);
+
+const markHigherBMI = markBmi > johnBmi;
+
+console.log(markHigherBMI);
