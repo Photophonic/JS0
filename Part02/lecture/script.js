c183ecf9a81a2309105a33a5bfc0ed09429e6e2e
Justin Bailey <sgm13555@gmail.com>
2023-03-06 20:27:27 -0600

    functions

--- Part02/lecture/script.js
+++ Part02/lecture/script.js
@@ -34,3 +34,36 @@ if (markBmi > johnBmi) {
 } else {
   console.log(`${johnBmi} John's BMI is higher than MArk's!`);
 }
+
+function describeCountry(country, population, capitalCity) {
+  return `${country} has a population of ${population} and its capital city is ${capitalCity}`;
+}
+
+let usPop = describeCountry("US", "6 million", "Washington DC");
+console.log(usPop);
+
+function calcAge1(birthYear) {
+  let year = new Date().getFullYear();
+  return year - birthYear;
+}
+
+const age1 = calcAge1(1980);
+
+var age2 = function (birthYear) {
+  let year = new Date().getFullYear();
+  return year - birthYear;
+};
+
+console.log(age1, age2(1980));
+
+let age3 = (birthyear) => 2023 - birthyear;
+
+console.log(age3(1980));
+
+const toRetire = (birthYear) => {
+  const age = 2023 - birthYear;
+  const retirement = 65 - age;
+  return retirement;
+};
+
+console.log(toRetire(1980));
