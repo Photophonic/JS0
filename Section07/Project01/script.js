3a80321dd3bfd60861f0f197f51b32f9be6425f4
Seth Martin <sgm13555@gmail.com>
2023-04-04 15:01:10 -0500

    Guess logic

--- Section07/Project01/script.js
+++ Section07/Project01/script.js
@@ -16,8 +16,23 @@ console.log(document.querySelector('.guess').value);
 
 //addEventListener requires two arguments. 1 - what it is listening for,
 //2 - what to do when activated. This example uses a function expression.
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+
 document.querySelector('.check').addEventListener('click', function () {
   //console.log(document.querySelector('.guess').value);
-  const guess = document.querySelector('.guess').value;
-  document.querySelector('.number').textContent = guess;
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = ' ⛔ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = ' 🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = ' 🤷‍♂️ Guess too high';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = ' 🤷 Guess too low';
+  }
+  //document.querySelector('.number').textContent = guess;
 });
