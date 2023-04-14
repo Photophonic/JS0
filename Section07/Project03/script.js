8655f0dfdc57707db5f8b7a41702f2ab232eb177
Seth Martin <sgm13555@gmail.com>
2023-04-14 15:58:51 -0500

    more code switch players

--- Section07/Project03/script.js
+++ Section07/Project03/script.js
@@ -1,13 +1,22 @@
 'use strict';
 
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
+
 //Since we're selecting an element, use the following instead of querySelector
 const score0 = document.getElementById('score--0');
 const score1 = document.getElementById('score--1');
+const currentScore0 = document.getElementById('current--0');
+const currentScore1 = document.getElementById('current--1');
 
 //dice image
 const dice = document.querySelector('.dice');
 
+//Create an array to store the score totals for each players
+const scores = [0, 0];
 let currentScore = 0;
+//Player 1 and 2
+let activePlayer = 0;
 
 //dice buttons
 const newGame = document.querySelector('.btn--new');
@@ -17,6 +26,8 @@ const hold = document.querySelector('.btn--hold');
 //set initial conditions
 score0.textContent = 0;
 score1.textContent = 0;
+currentScore0.textContent = 0;
+currentScore1.textContent = 0;
 
 //remember, don't use . before the class name for add/removeq
 dice.classList.add('hidden');
@@ -33,10 +44,20 @@ rollDice.addEventListener('click', function () {
   if (diceRoll !== 1) {
     //add roll to score
     currentScore += diceRoll;
-    console.log(currentScore);
+    //use ${} to dynamically update player based on activePlayer variable
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    //switch to next player
+    //reset scores to 0
     currentScore = 0;
-    console.log(currentScore);
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+
+    //switch to next player using ternary (like a decode in SQL)
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    //switch active players with CSS class
+    player0.classList.toggle('player--active');
+    player1.classList.toggle('player--active');
   }
 });
