0be7a2a4ede19cecbf3b9f4756ebb1344135b9c8
Seth Martin <sgm13555@gmail.com>
2023-05-16 14:39:30 -0500

    section 9 redo

--- Section09/DataStructures/script.js
+++ Section09/DataStructures/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,43 @@ const restaurant = {
     },
   },
 };
+
+//Video 103 destructuring arrays
+//without destructuring
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+console.log('previous ', a, b, c);
+
+//with destructuring (unpacking)
+const [x, y, z] = arr;
+console.log('destuctured ', x, y, z);
+
+//to target specific items from an object. to skip, leave a gap.
+const [first, , third] = restaurant.categories;
+
+console.log(first, third);
+
+//to swtich order of categories, swap the order of declare variables
+const [secondary, , main] = restaurant.categories;
+console.log(main, secondary);
+
+//Call the method in the object to return an array of food items to destructure
+const [sideDish, mainDish] = restaurant.order(2, 0);
+console.log(sideDish, mainDish);
+
+//nested arrays for destructuring
+const nested = [2, 3, [5, 6]];
+//make an inner array variable group to capture the nested array from nested
+const [d, , [e, f]] = nested;
+
+console.log(d, e, f);
+
+//to set default variables for destructuring
+const defaultArr = [8, 9];
+//place an = SOMETHING to set a default value in case the incoming array is smaller
+const [p = 1, q = 1, r = 1] = defaultArr;
+console.log(p, q, r);
+
+//video 104 destructing objects
